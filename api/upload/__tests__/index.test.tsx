94b555a1198178b938192fd0cda5fe042dc87181
mizadi <adi.mizrahi@cloudinary.com>
2023-05-14 14:28:32 +0300

    Start working on network layer

---
+++ api/upload/__tests__/index.test.tsx
@@ -0,0 +1,10 @@
+// import { apiRequest } from '../uploader.js';
+
+
+// const IMAGE_FILE = require('../__tests__/.resources/logo.png');
+
+// describe('AdvancedImage', () => {
+//     it('should render an Image with the correct URI', () => {
+//         apiRequest();
+//     });
+// });
