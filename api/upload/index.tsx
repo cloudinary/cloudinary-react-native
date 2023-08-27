655cf4116d0aac057947ba4562cb550bd8a9bda9
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2023-08-27 10:54:21 +0300

    Fix api export

---
+++ api/upload/index.tsx
@@ -0,0 +1,5 @@
+import { upload, unsignedUpload, uploadBase64, rename, explicit } from "./src/uploader";
+import { UploadApiOptions } from './model/params/upload-params';
+
+export { upload, unsignedUpload, uploadBase64, rename, explicit };
+export { UploadApiOptions };
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
 import AdvancedImage from "./AdvancedImage";
+export { upload, unsignedUpload, uploadBase64, rename, explicit } from "./../api/upload";
+export { UploadApiOptions } from './../api/upload/model/params/upload-params';
+
 export { AdvancedImage };
