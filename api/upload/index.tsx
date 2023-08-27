8b017f0e0a2149641918a11e515b5ecd9012cea2
adimiz1 <adi.mizrahi@cloudinary.com>
2023-08-27 08:54:12 +0300

    Fix export for api package

---
+++ api/upload/index.tsx
@@ -0,0 +1,6 @@
+import { upload, unsignedUpload, uploadBase64, rename, explicit } from "./src/uploader";
+import { UploadApiOptions } from './model/params/upload-params';
+
+export { upload, unsignedUpload, uploadBase64, rename, explicit };
+export { UploadApiOptions };
+
