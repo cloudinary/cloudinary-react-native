eae6c7a3371b623fb514bb8b601aea519c216c79
adimiz1 <adi.mizrahi@cloudinary.com>
2023-09-04 13:49:53 +0300

    Fix api location

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import AdvancedImage from "./AdvancedImage";
-export { upload, unsignedUpload, uploadBase64, rename, explicit } from "./../api/upload";
-export { UploadApiOptions } from './../api/upload/model/params/upload-params';
+export { upload, unsignedUpload, uploadBase64, rename, explicit } from "./api/upload";
+export { UploadApiOptions } from './api/upload/model/params/upload-params';
 
 export { AdvancedImage };
