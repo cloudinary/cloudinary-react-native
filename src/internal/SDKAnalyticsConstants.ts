394b3cb4d1d5af3b86427bbc6d6b84b3bd82d792
adimiz1 <adi.mizrahi@cloudinary.com>
2024-05-06 12:54:05 +0300

    Add analytics algo D support

--- src/internal/SDKAnalyticsConstants.ts
+++ src/internal/SDKAnalyticsConstants.ts
@@ -5,25 +5,48 @@ const getReactNativeVersion = () => {
     const version = Platform.Version;
     return version.toString();
   } catch {
-    return "0.0.0"
+    return '0.0.0';
   }
-}
+};
 
 const getSDKVersion = () => {
   try{
-    const SDKVersionPackageJson = require('../../package.json')
+    const SDKVersionPackageJson = require('../../package.json');
     if (SDKVersionPackageJson && SDKVersionPackageJson.version) {
-      //return SDKVersionPackageJson.version
-      return
+      return SDKVersionPackageJson.version;
     }
   } catch {
-    return "0.0.0";
+    return '0.0.0';
   }
-  return "0.0.0";
-}
+  return '0.0.0';
+};
+
+const getOSType = () => {
+  switch (Platform.OS) {
+    case 'android':
+      return 'A';
+    case 'ios':
+      return 'B';
+    default:
+      return 'Z';
+  }
+};
+
+const getOSVersion = () => {
+  switch (Platform.OS) {
+    case 'android':
+      return Platform.Version ? Platform.Version.toString() : 'AA';
+    case 'ios':
+      return Platform.Version ? Platform.Version.toString() : 'AA';
+    default:
+      return 'AA';
+  }
+};
 
 export const SDKAnalyticsConstants = {
   sdkSemver: getSDKVersion(),
   techVersion: getReactNativeVersion(),
-  sdkCode: 'P'
+  sdkCode: 'P',
+  osType: getOSType(),
+  osVersion: getOSVersion(),
 };
