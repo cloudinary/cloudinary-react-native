32a9e938d152adf2a26de420f491e68672bf6dac
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2024-05-07 07:57:20 +0300

    Update analytics token

--- src/__tests__/analytics.test.tsx
+++ src/__tests__/analytics.test.tsx
@@ -1,5 +1,5 @@
 import { SDKAnalyticsConstants } from '../internal/SDKAnalyticsConstants';
-import { Image } from 'react-native';
+import { Image, Platform } from 'react-native';
 import AdvancedImage from '../AdvancedImage';
 import { CloudinaryImage } from '@cloudinary/url-gen/assets/CloudinaryImage';
 import { render } from '@testing-library/react-native';
@@ -7,14 +7,25 @@ import React from 'react';
 
 const cloudinaryImage = new CloudinaryImage('sample', { cloudName: 'demo' });
 
+
 describe('analytics', () => {
-    beforeEach(() => {
-      SDKAnalyticsConstants.sdkSemver = '1.0.0';
-      SDKAnalyticsConstants.techVersion = '10.2.5';
-    });
-    it('creates a url with analytics', () => {
-        const element = render(<AdvancedImage cldImg={cloudinaryImage}></AdvancedImage>);
-        const imageComponent = element.root.findByType(Image);
-        expect(imageComponent.props.source.uri).toBe(cloudinaryImage.toURL({trackedAnalytics: SDKAnalyticsConstants}));
-    });
+  beforeEach(() => {
+    SDKAnalyticsConstants.sdkSemver = '1.0.0';
+    SDKAnalyticsConstants.techVersion = '10.2.5';
+    SDKAnalyticsConstants.osType = 'A';
+    SDKAnalyticsConstants.osVersion = '30';
   });
+  it('creates a url with analytics', () => {
+    const element = render(<AdvancedImage cldImg={cloudinaryImage}></AdvancedImage>);
+    const imageComponent = element.root.findByType(Image);
+    expect(imageComponent.props.source.uri).toBe(cloudinaryImage.toURL({trackedAnalytics: SDKAnalyticsConstants}));
+  });
+
+  it('sets correct osType', () => {
+    expect(SDKAnalyticsConstants.osType).toBe('A'); // For Android
+  });
+
+  it('sets correct osVersion', () => {
+    expect(SDKAnalyticsConstants.osVersion).toBe('30'); // Mocked Android version (API level)
+  });
+});
